76d8aa48ac95e2c852cda90e0aff2fc2300d2694
Ekaterina Ivashkevich <81188812+Ekaterina-Ivashkevich@users.noreply.github.com>
2021-04-04 14:16:58 +0300

    Add files via upload

---
+++ lesson15.js
@@ -0,0 +1,63 @@
+var myNum = 10;
+console.log(myNum);
+var myStr = 'строка';
+console.log(myStr);
+var myBool = true;
+console.log(myBool);
+var myArr = [1, 2, 3, 4, 5];
+console.log(myArr);
+var myObj = {
+    first: 'First Name',
+    last: 'Last Name'
+};
+console.log(myObj);
+
+var decimal2 = myNum.toFixed(2);
+
+var i = 12;
+console.log(++i);
+console.log(++i);
+console.log(i++);
+console.log(i++);
+console.log(i);
+console.log(--i);
+console.log(--i);
+console.log(i--);
+console.log(i--);
+
+var myTest = 20;
+myTest += 10;
+console.log(myTest);
+myTest -= 10;
+console.log(myTest);
+myTest *= 10;
+console.log(myTest);
+myTest /= 10;
+console.log(myTest);
+myTest %= 4;
+console.log(myTest);
+
+var myPi = Math.PI;
+console.log(myPi);
+var myRound = Math.round(89.279);
+console.log(myRound);
+var myRandom = Math.random() * 10;
+console.log(myRandom);
+var myPow = Math.pow(3, 5);
+console.log(myPow);
+
+var strObj = { str: 'Мама мыла раму, рама мыла маму' };
+strObj.length = strObj.str.length;
+console.log(strObj.lenght);
+
+var isRamaPos = strObj.str.indexOf('рама');
+console.log(isRamaPos);
+
+var strReplace = strObj.str.replace('мыла', 'моет');
+strReplace = strReplace.replace('рама', 'Рама');
+strReplace = strReplace.replace('мыла', 'держит');
+console.log(strReplace);
+
+var string = 'Любая строка'
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
